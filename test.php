712bbea54184bb35b1d67657d3394973fa6f77c6
Ashbee Morgado <ashbee.morgado@icloud.com>
2020-02-14 21:21:52 +0800

    first commit

---
+++ test.php
@@ -0,0 +1,5 @@
+<?php
+
+$ip = isset($_SERVER['HTTP_CLIENT_IP']) ? $_SERVER['HTTP_CLIENT_IP'] : isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : $_SERVER['REMOTE_ADDR'];
+
+echo $ip;
