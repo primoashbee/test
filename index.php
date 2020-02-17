a703f5fac6189478fc114ed4764ca5bfad10a0c0
Ashbee Morgado <ashbee.morgado@icloud.com>
2020-02-17 10:45:40 +0800

    gg
    gg

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 
 require "vendor/autoload.php";
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 $ip = isset($_SERVER['HvendorTTP_CLIENT_IP']) ? $_SERVER['HTTP_CLIENT_IP'] : isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : $_SERVER['REMOTE_ADDR'];
 $email = new \SendGrid\Mail\Mail(); 
 
@@ -10,10 +13,12 @@ $email->setSubject("Sending with SendGrid is Fun");
 $email->addTo("ashbee.morgado@icloud.com", "Ashbee Morgado - iCloud");
 $email->addContent("text/plain", $ip);
 
-$sendgrid = new \SendGrid(( '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'));
+$sendgrid = new \SendGrid(getenv('SendGrid_KEY'));
 try {
     $response = $sendgrid->send($email);
-
+    print $response->statusCode() . "\n";
+    print_r($response->headers());
+    print $response->body() . "\n";
 } catch (Exception $e) {
-    //echo 'Caught exception: '. $e->getMessage() ."\n";
+    echo 'Caught exception: '. $e->getMessage() ."\n";
 }
